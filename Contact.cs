6737d6ee84bf21133fa14b7e8adf27761931a8e6
Oleksandr Shevchenko <105636389+AlexShevchenk@users.noreply.github.com>
2023-03-14 21:09:47 +0200

    File Collections

---
+++ Contact.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M3H4
+{
+ class Contact
+{
+    public string Name { get; set; }
+    public string Phone { get; set; }
+
+    public CultureInfo Culture { get; set; }
+
+    public Contact(string name, string phone, CultureInfo culture)
+    {
+    Name = name;
+    Phone = phone;
+    Culture = culture;
+    }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace M3H4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContactList contacts = new ContactList();
+            contacts.Add(new Contact("John Smith", "1234567890", CultureInfo.GetCultureInfo("en-US")));
+            contacts.Add(new Contact("Іван Петров", "0987654321", CultureInfo.GetCultureInfo("uk-UA")));
+            contacts.Add(new Contact("Анна Коваль", "5551234", CultureInfo.GetCultureInfo("uk-UA")));
+            contacts.Add(new Contact("Вікторія Петренко", "2223334444", CultureInfo.GetCultureInfo("uk-UA")));
+            Console.WriteLine(contacts);
+        }
+    }
+}
